46f103bcb06733c5a8a84fec9e235c578bbe1d7e
Glitch (wwsc-20-dev) <none>
2020-08-08 16:44:18 +0000

    🍈🌋 Checkpoint ./index.html:3854026/4827 ./ad.js:3854026/968 ./style.css:3854026/114

---
+++ ad.js
@@ -0,0 +1,7 @@
+document.querySelector(".ad").innerHTML=location.host=="wwsc20.github.io"?
+  `<ins class="adsbycontextcue" data-cc-site="b27b9ca4-206c-4100-952b-372c5d5b44ef" data-cc-slot="51Lvt7VpB"></ins>`:
+
+var script = document.createElement('script');
+script.src = "https://cdn.contextcue.com/adsbycontextcue.js";
+script.async=true;
+document.head.appendChild(script);
