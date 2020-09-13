27c72f33fed3418f3ef17b254aca8ea18b081462
Glitch (wwsc-20-dev) <none>
2020-09-13 21:56:13 +0000

    stuff

--- ad.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".ad").innerHTML =
-  location.host == "wwsc20.github.io"
-    ? `<ins class="adsbycontextcue" data-cc-site="b27b9ca4-206c-4100-952b-372c5d5b44ef" data-cc-slot="51Lvt7VpB"></ins>`
-    : `<ins class="adsbycontextcue" data-cc-site="886fd17c-b3bd-43ad-8d1b-5078d78dac01" data-cc-slot="V7VQIFsj"></ins>`;
-
-var script = document.createElement("script");
-script.src = "https://cdn.contextcue.com/adsbycontextcue.js";
-script.async = true;
-document.head.appendChild(script);
